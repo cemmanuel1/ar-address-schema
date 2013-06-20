e47cae7260f72c5e39f0b86bcff92f7759231320
Apprentice <apprentice@dbc07.local>
2013-06-19 18:29:04 -0700

    solo done

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,16 @@
 require_relative '../../db/config'
 
-class Contact< ActiveRecord::Base
+class Contact < ActiveRecord::Base
+  validates :email, :format => { :with => /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/ }
+  validates :phone, :format => { :with => /^[0-9]{3}.?-?[0-9]{3}.?-?[0-9]{4}$/}
+  validates :company, :presence => true
 
-# implement your Contact model here
+  def name
+    first_name + " " + last_name
+  end
+
+  def area_code 
+    phone_number.match(/^\(?[0-9][0-9][0-9]\)?/)
+  end 
 
 end
